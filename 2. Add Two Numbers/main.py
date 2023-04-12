68e91d7dcd0597a5d6f5e94aacd599bda6981e43
fidasek009 <fidasek009@gmail.com>
2023-04-12 23:54:03 +0200

    kys

--- 2. Add Two Numbers/main.py
+++ 2. Add Two Numbers/main.py
@@ -1,1 +1,11 @@
-a
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    pass
