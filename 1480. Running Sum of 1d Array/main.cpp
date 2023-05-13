ff9b797e6a65f1607f872c22e89ee2938eca553a
fidasek009 <fidasek009@gmail.com>
2023-05-13 11:14:42 +0200

    1480. C++ beats 100%

--- 1480. Running Sum of 1d Array/main.cpp
+++ 1480. Running Sum of 1d Array/main.cpp
@@ -1,8 +1,26 @@
 #include <vector>
+#include <iostream>
+
 
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
-        
+    std::vector<int> runningSum(std::vector<int>& nums) {
+        for(unsigned int i = 1; i < nums.size(); i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
     }
 };
+
+
+int main(int argc, char const *argv[])
+{
+    std::vector inp = {1,2,3,4};
+    Solution s = Solution();
+    std::vector<int> res = s.runningSum(inp);
+
+    for(unsigned int i = 0; i < res.size(); i++){
+        std::cout << res[i] << ", ";
+    }
+    return 0;
+}
